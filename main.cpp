ff7b8a33f049833e5bdbb5edba1cc680d78e617a
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-02-11 16:36:28 +0100

    First tries using a movies database - doesn't work for now

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
 #include <QApplication>
-#include <QMessageBox>
 #include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+
+void tryDatabase() {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "connection_name");
+    db.setDatabaseName("test_sqlite.db");
+    if (!db.open()) {
+        qDebug() << "Database open error";
+    }
+    if (!db.isOpen() ) {
+        qDebug() << "Database is not open";
+    }
+    QSqlQuery query(db);
+    // qDebug() << query.;
+    query.exec("SELECT first_name, last_name FROM actor;");
+
+    while (query.next()) {
+        qDebug() << query.value(1).toString() << "\n";
+    }
+
+    qDebug() << "End";
+}
 
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
-    qDebug() << "Hello Git from Qt Creator!";
+    tryDatabase();
+    return 0;
     return app.exec();
 
 }
--- netatmo-w-analysis.pro
+++ netatmo-w-analysis.pro
@@ -1,4 +1,5 @@
 QT += widgets
+QT += sql
 
 SOURCES += \
     main.cpp
