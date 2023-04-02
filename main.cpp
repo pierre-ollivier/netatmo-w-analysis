a95935f4f19529f2d53b0dfd094bb939a2ce0856
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-04-02 11:48:30 +0200

    Add a MainWindow class and instantiate it

--- main.cpp
+++ main.cpp
@@ -1,11 +1,62 @@
 #include <QApplication>
 #include <QMessageBox>
 #include <QDebug>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QWidget>
+#include <QtWidgets>
+#include <QNetworkRequest>
+#include <QNetworkAccessManager>
+#include "frontend/MainWindow.h"
+
+
+void tryDatabase() {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "connection_name");
+    db.setDatabaseName("../netatmo-w-analysis/test_sqlite.db");
+    if (!db.open()) {
+        qDebug() << "Database open error";
+    }
+    if (!db.isOpen() ) {
+        qDebug() << "Database is not open";
+    }
+    qDebug() << db.tables();
+    QSqlQuery query(db);
+    query.exec("SELECT first_name, last_name FROM actor");
+    qDebug() << query.next();
+
+    while (query.next()) {
+        qDebug() << query.value(1).toString();
+    }
+
+    qDebug() << "End";
+}
+
+void tryDatabase2() {
+    QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE", "connection_name");
+    db.setDatabaseName("../netatmo-w-analysis/netatmo_analysis.db");
+    if (!db.open()) {
+        qDebug() << "Database open error";
+    }
+    if (!db.isOpen() ) {
+        qDebug() << "Database is not open";
+    }
+    qDebug() << db.tables();
+    QSqlQuery query(db);
+    //query.exec("INSERT INTO DailyRecords VALUES (null,null,null,null);");
+    query.prepare("INSERT INTO DailyRecords(Date,Tx,Tn) VALUES (?,?,?);");
+    //query.addBindValue(14);
+    query.addBindValue(1678791600);
+    query.addBindValue(13.8);
+    query.addBindValue(5.1);
+    query.exec();
+}
 
 int main(int argc, char *argv[]) {
 
     QApplication app(argc, argv);
-    qDebug() << "Hello Git from Qt Creator!";
+//    tryDatabase2();
+    MainWindow mainWin;
+    mainWin.show();
     return app.exec();
 
 }
--- netatmo-w-analysis.pro
+++ netatmo-w-analysis.pro
@@ -1,4 +1,10 @@
 QT += widgets
+QT += network
+QT += sql
+
+include (types/types.pri)
+include (frontend/frontend.pri)
+include (backend/backend.pri)
 
 SOURCES += \
     main.cpp
