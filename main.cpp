3c759a17982c678517e6abc22552357b719da1c7
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-04-16 17:33:17 +0200

    Push the newly retrieved timestamp records to the database

--- main.cpp
+++ main.cpp
@@ -1,12 +1,4 @@
 #include <QApplication>
-#include <QMessageBox>
-#include <QDebug>
-#include <QSqlDatabase>
-#include <QSqlQuery>
-#include <QWidget>
-#include <QtWidgets>
-#include <QNetworkRequest>
-#include <QNetworkAccessManager>
 #include "frontend/MainWindow.h"
 #include "backend/DatabaseHandler.h"
 #include "types/ExtDailyRecord.h"
@@ -14,7 +6,7 @@
 
 void tryDatabase2() {
     DatabaseHandler dbHandler("netatmo_analysis.db");
-    dbHandler.postDailyRecord(
+    dbHandler.postOutdoorDailyRecord(
                 ExtDailyRecord(
                     QDate(2023, 4, 14),
                     9.7,
