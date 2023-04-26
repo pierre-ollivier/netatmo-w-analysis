76a5808712baa3539c7940fedac50460f1e1db3b
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-04-26 21:58:08 +0200

    Calculate the maximum temperature of a given date (using a standardized time window)

---
+++ backend/DailyStatisticsCalculator.h
@@ -0,0 +1,22 @@
+#ifndef DAILYSTATISTICSCALCULATOR_H
+#define DAILYSTATISTICSCALCULATOR_H
+
+#include <QDate>
+#include "DatabaseHandler.h"
+
+class DailyStatisticsCalculator
+{
+public:
+    DailyStatisticsCalculator();
+    double getMaxTemperatureFromDate(QDate date, bool indoor = false);
+    long long getMaxTemperatureTimestampFromDate(QDate date, double maxTemperature, bool indoor = false);
+    long long getMaxTemperatureTimestampFromDate(QDate date, bool indoor = false);
+
+    long long getFirstTimestampFromDate(QDate date);
+    long long getFirstTimestampFromDateWithUTCOffset(QDate date, int offsetFromUTCInHours);
+
+private:
+    DatabaseHandler *dbHandler;
+};
+
+#endif // DAILYSTATISTICSCALCULATOR_H
--- backend/backend.pri
+++ backend/backend.pri
@@ -1,9 +1,11 @@
 HEADERS += \
     $$PWD/APIMonitor.h \
+    $$PWD/DailyStatisticsCalculator.h \
     $$PWD/DatabaseHandler.h \
     $$PWD/NetatmoAPIHandler.h
 
 SOURCES += \
     $$PWD/APIMonitor.cpp \
+    $$PWD/DailyStatisticsCalculator.cpp \
     $$PWD/DatabaseHandler.cpp \
     $$PWD/NetatmoAPIHandler.cpp
