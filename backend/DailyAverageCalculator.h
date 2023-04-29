9ed88c13ebbba0a63836862d134a29de165f6543
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-04-29 21:12:06 +0200

    Add DailyAverageCalculator class

---
+++ backend/DailyAverageCalculator.h
@@ -0,0 +1,14 @@
+#ifndef DAILYAVERAGECALCULATOR_H
+#define DAILYAVERAGECALCULATOR_H
+
+
+class DailyAverageCalculator
+{
+public:
+    DailyAverageCalculator(bool indoor);
+
+private:
+    bool _indoor;
+};
+
+#endif // DAILYAVERAGECALCULATOR_H
