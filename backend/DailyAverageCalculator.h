173a451ab8a776479bd815fe32a888e15d3cc033
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-04-30 16:15:49 +0200

    Calculate average indoor temperature

--- backend/DailyAverageCalculator.h
+++ backend/DailyAverageCalculator.h
@@ -9,6 +9,11 @@ public:
     DailyAverageCalculator(bool indoor);
     double getAverageExtTemperatureFromDate(QDate date);
 
+    double getFirstTemperatureFromDate(QDate date);
+    double getLastTemperatureFromDate(QDate date);
+    long long getFirstTimestampFromDate(QDate date);
+    long long getLastTimestampFromDate(QDate date);
+
 private:
     bool _indoor;
 };
