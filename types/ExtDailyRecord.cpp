5ebe8d9ed408a3c0c91c561c2f2e60ca53dab687
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-04 22:59:09 +0200

    Update ExtDailyRecord to add the new measurements

--- types/ExtDailyRecord.cpp
+++ types/ExtDailyRecord.cpp
@@ -8,10 +8,20 @@ ExtDailyRecord::ExtDailyRecord(
         int maxHumidity,
         int minHumidity,
         double avgHumidity,
+        double maxDewPoint,
+        double minDewPoint,
+        double avgDewPoint,
+        double maxHumidex,
+        double minHumidex,
+        double avgHumidex,
         long long maxTemperatureTimestamp,
         long long minTemperatureTimestamp,
         long long maxHumidityTimestamp,
-        long long minHumidityTimestamp
+        long long minHumidityTimestamp,
+        long long maxDewPointTimestamp,
+        long long minDewPointTimestamp,
+        long long maxHumidexTimestamp,
+        long long minHumidexTimestamp
         ) : DailyRecord(
                 date,
                 maxTemperature,
@@ -20,10 +30,20 @@ ExtDailyRecord::ExtDailyRecord(
                 maxHumidity,
                 minHumidity,
                 avgHumidity,
+                maxDewPoint,
+                minDewPoint,
+                avgDewPoint,
+                maxHumidex,
+                minHumidex,
+                avgHumidex,
                 maxTemperatureTimestamp,
                 minTemperatureTimestamp,
                 maxHumidityTimestamp,
-                minHumidityTimestamp
+                minHumidityTimestamp,
+                maxDewPointTimestamp,
+                minDewPointTimestamp,
+                maxHumidexTimestamp,
+                minHumidexTimestamp
                 )
 {
 
