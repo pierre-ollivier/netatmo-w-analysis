01319f342aacc43a8d9712c30b73b851103eb7c5
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-04 22:48:08 +0200

    Add dew point and humidex to DailyRecord

--- types/DailyRecord.h
+++ types/DailyRecord.h
@@ -15,10 +15,20 @@ public:
             int maxHumidity,
             int minHumidity,
             double avgHumidity,
+            double maxDewPoint,
+            double minDewPoint,
+            double avgDewPoint,
+            double maxHumidex,
+            double minHumidex,
+            double avgHumidex,
             long long maxTemperatureTimestamp,
             long long minTemperatureTimestamp,
             long long maxHumidityTimestamp,
-            long long minHumidityTimestamp
+            long long minHumidityTimestamp,
+            long long maxDewPointTimestamp,
+            long long minDewPointTimestamp,
+            long long maxHumidexTimestamp,
+            long long minHumidexTimestamp
             );
 
     QDate date();
@@ -38,17 +48,37 @@ public:
     int minHumidity();
     double avgHumidity();
 
+    double maxDewPoint();
+    double minDewPoint();
+    double avgDewPoint();
+
+    double maxHumidex();
+    double minHumidex();
+    double avgHumidex();
+
     long long maxTemperatureTimestamp();
     long long minTemperatureTimestamp();
 
     long long maxHumidityTimestamp();
     long long minHumidityTimestamp();
 
+    long long maxDewPointTimestamp();
+    long long minDewPointTimestamp();
+
+    long long maxHumidexTimestamp();
+    long long minHumidexTimestamp();
+
     QTime maxTemperatureTime();
     QTime minTemperatureTime();
 
     QTime maxHumidityTime();
     QTime minHumidityTime();
+
+    QTime maxDewPointTime();
+    QTime minDewPointTime();
+
+    QTime maxHumidexTime();
+    QTime minHumidexTime();
 
 private:
     QDate _date;
@@ -61,13 +91,25 @@ private:
     int _minHumidity;
     double _avgHumidity;
 
+    double _maxDewPoint;
+    double _minDewPoint;
+    double _avgDewPoint;
+
+    double _maxHumidex;
+    double _minHumidex;
+    double _avgHumidex;
+
     long long _maxTemperatureTimestamp;
     long long _minTemperatureTimestamp;
 
     long long _maxHumidityTimestamp;
     long long _minHumidityTimestamp;
 
+    long long _maxDewPointTimestamp;
+    long long _minDewPointTimestamp;
 
+    long long _maxHumidexTimestamp;
+    long long _minHumidexTimestamp;
 
 };
 
