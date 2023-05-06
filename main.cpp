93fb9a3df70b1268778de987a643b34f36de1216
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-06 22:58:36 +0200

    Add version number to window title

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     MainWindow mainWin;
     mainWin.show();
+    mainWin.setWindowTitle("netatmo-w-analysis v1.0.0");
     executeAllPlaygroundFunctions();
     return app.exec();
-
 }
