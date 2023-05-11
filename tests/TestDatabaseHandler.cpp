358a62e4fcdb3b835a620abd97a786c24ee623ab
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-11 21:33:32 +0200

    Add tests for DatabaseHandler::getDailyRecordsFromDatabase

--- tests/TestDatabaseHandler.cpp
+++ tests/TestDatabaseHandler.cpp
@@ -1,6 +1,5 @@
 #include "TestDatabaseHandler.h"
 #include <QTest>
-#include "../netatmo-w-analysis/types/IntTimestampRecord.h"
 #include "../netatmo-w-analysis/backend/DatabaseHandler.h"
 
 TestDatabaseHandler::TestDatabaseHandler()
@@ -19,4 +18,19 @@ void TestDatabaseHandler::testGetTimestampRecordsFromDatabase() {
     QCOMPARE(extRecords[0].temperature(), 17.2);
     QCOMPARE(extRecords[1].temperature(), 17.2);
     QCOMPARE(extRecords[2].temperature(), 17.3);
+}
+
+void TestDatabaseHandler::testGetDailyRecordsFromDatabase() {
+    DatabaseHandler dbHandler("testdb.db");
+    std::vector<IntDailyRecord> records = dbHandler.getIntDailyRecordsFromDatabase(
+                "SELECT * from IndoorDailyRecords WHERE weekNumber = 41 ORDER BY id", -1);
+    QCOMPARE(records.size(), 7);
+    QCOMPARE(records[0].maxTemperature(), 21.8);
+    QCOMPARE(records[1].minHumidity(), 58);
+    QCOMPARE(records[2].maxCO2(),1219);
+    QCOMPARE(records[3].maxNoise(), 51);
+    std::vector<ExtDailyRecord> extRecords = dbHandler.getExtDailyRecordsFromDatabase(
+                "SELECT * from OutdoorDailyRecords WHERE year = 2019 AND month = 10 ORDER BY day DESC", 1);
+    QCOMPARE(extRecords.size(), 1);
+    QCOMPARE(extRecords[0].minTemperature(), 9);
 }
