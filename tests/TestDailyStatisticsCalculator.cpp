5af46da7514a5dd97cdd0fe605e6e3bc5c76f73c
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-12 23:01:19 +0200

    Add temperature tests to TestDailyStatisticsCalculator

--- tests/TestDailyStatisticsCalculator.cpp
+++ tests/TestDailyStatisticsCalculator.cpp
@@ -1,4 +1,6 @@
 #include "TestDailyStatisticsCalculator.h"
+#include <QTest>
+#include <QDate>
 
 TestDailyStatisticsCalculator::TestDailyStatisticsCalculator()
 {
@@ -8,21 +10,30 @@ TestDailyStatisticsCalculator::TestDailyStatisticsCalculator()
 //temperature
 
 void TestDailyStatisticsCalculator::testGetMaxTemperatureFromDate() {
-
+    QCOMPARE(calculator->getMaxTemperatureFromDate(QDate(2019, 10, 6)), 17.8);
+    QCOMPARE(calculator->getMaxTemperatureFromDate(QDate(2019, 10, 6), true), 21.7);
 }
 void TestDailyStatisticsCalculator::testGetMaxTemperatureTimestampFromDate() {
-
+    QCOMPARE(calculator->getMaxTemperatureTimestampFromDate(QDate(2019, 10, 6)), 1570366928);
+    QCOMPARE(calculator->getMaxTemperatureTimestampFromDate(QDate(2019, 10, 6), 17.8), 1570366928);
+    QCOMPARE(calculator->getMaxTemperatureTimestampFromDate(QDate(2019, 10, 6), true), 1570376108);
+    QCOMPARE(calculator->getMaxTemperatureTimestampFromDate(QDate(2019, 10, 6), 21.7, true), 1570376108);
 }
 
 void TestDailyStatisticsCalculator::testGetMinTemperatureFromDate() {
-
+    QCOMPARE(calculator->getMinTemperatureFromDate(QDate(2019, 10, 7)), 13.2);
+    QCOMPARE(calculator->getMinTemperatureFromDate(QDate(2019, 10, 7), true), 18.3);
 }
 void TestDailyStatisticsCalculator::testGetMinTemperatureTimestampFromDate() {
-
+    QCOMPARE(calculator->getMinTemperatureTimestampFromDate(QDate(2019, 10, 7)), 1570428760);
+    QCOMPARE(calculator->getMinTemperatureTimestampFromDate(QDate(2019, 10, 7), 13.2), 1570428760);
+    QCOMPARE(calculator->getMinTemperatureTimestampFromDate(QDate(2019, 10, 7), true), 1570428185);
+    QCOMPARE(calculator->getMinTemperatureTimestampFromDate(QDate(2019, 10, 7), 18.3, true), 1570428185);
 }
 
 void TestDailyStatisticsCalculator::testGetAvgTemperatureFromDate() {
-
+    QCOMPARE(calculator->getAvgTemperatureFromDate(QDate(2019, 10, 8)), 14.4644872685185);
+    QCOMPARE(calculator->getAvgTemperatureFromDate(QDate(2019, 10, 8), true), 20.8567164351852);
 }
 
 // humidity
