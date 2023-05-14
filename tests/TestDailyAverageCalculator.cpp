eb9fb0f632b2d4bc18b4d66c85b70132f9530386
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-14 21:20:47 +0200

    Add tests for getLastMeasurementFromDate

--- tests/TestDailyAverageCalculator.cpp
+++ tests/TestDailyAverageCalculator.cpp
@@ -23,7 +23,19 @@ void TestDailyAverageCalculator::testGetFirstMeasurementFromDate() {
     QCOMPARE(outdoorCalculator->getFirstMeasurementFromDate(testDate, "humidex"), 9.60855251462557);
 }
 void TestDailyAverageCalculator::testGetLastMeasurementFromDate() {
+    QDate testDate = QDate(2019, 10, 31);
+    QCOMPARE(indoorCalculator->getLastMeasurementFromDate(testDate, "temperature"), 20.7);
+    QCOMPARE(indoorCalculator->getLastMeasurementFromDate(testDate, "humidity"), 56);
+    QCOMPARE(indoorCalculator->getLastMeasurementFromDate(testDate, "dewPoint"), 11.5672449958022);
+    QCOMPARE(indoorCalculator->getLastMeasurementFromDate(testDate, "humidex"), 22.7372408785317);
+    QCOMPARE(indoorCalculator->getLastMeasurementFromDate(testDate, "pressure"), 1016.6);
+    QCOMPARE(indoorCalculator->getLastMeasurementFromDate(testDate, "co2"), 965);
+    QCOMPARE(indoorCalculator->getLastMeasurementFromDate(testDate, "noise"), 37);
 
+    QCOMPARE(outdoorCalculator->getLastMeasurementFromDate(testDate, "temperature"), 11.5);
+    QCOMPARE(outdoorCalculator->getLastMeasurementFromDate(testDate, "humidity"), 90);
+    QCOMPARE(outdoorCalculator->getLastMeasurementFromDate(testDate, "dewPoint"), 9.89920702513028);
+    QCOMPARE(outdoorCalculator->getLastMeasurementFromDate(testDate, "humidex"), 12.7318686435551);
 }
 void TestDailyAverageCalculator::testGetFirstTimestampFromDate() {
 
