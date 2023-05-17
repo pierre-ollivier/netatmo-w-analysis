f7611a4e8750d6557fbe47beafa937869b8831b2
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-17 23:19:45 +0200

    Remove legacy commented code

--- netatmo-w-analysis/frontend/YearMonthPicker.cpp
+++ netatmo-w-analysis/frontend/YearMonthPicker.cpp
@@ -3,22 +3,6 @@
 
 YearMonthPicker::YearMonthPicker(int baseYear, int baseMonth, QWidget *parent) : QWidget(parent)
 {
-//    monthButtonsLayout = new QGridLayout();
-//    for (int i = 0; i < 3; i++) {
-//        for (int j = 0; j < 4; j++) {
-//            monthButtons[3 * j + i] = new QPushButton(abbreviatedMonths[3 * j + i]);
-//            monthButtonsLayout->addWidget(monthButtons[3 * j + i], j, i);
-//        }
-//    }
-//    yearButtonsLayout = new QVBoxLayout();
-//    for (int i = 0; i < 5; i++) {
-//        yearButtons[i] = new QPushButton(QString::number(2019 + i));
-//        yearButtonsLayout->addWidget(yearButtons[i]);
-//    }
-//    mainLayout = new QHBoxLayout();
-//    mainLayout->addLayout(yearButtonsLayout);
-//    mainLayout->addLayout(monthButtonsLayout);
-
     monthModel = new QStandardItemModel();
     yearModel = new QStandardItemModel();
 
