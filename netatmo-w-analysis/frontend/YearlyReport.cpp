1477a71b432cb4e0ddd1f5fcdcec5b7e8f474001
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-21 21:51:51 +0200

    Fill the table with the maximum maximum temperature of each day

--- netatmo-w-analysis/frontend/YearlyReport.cpp
+++ netatmo-w-analysis/frontend/YearlyReport.cpp
@@ -1,5 +1,6 @@
 #include "YearlyReport.h"
 #include <QDate>
+#include <QDebug>
 
 YearlyReport::YearlyReport()
 {
@@ -7,6 +8,8 @@ YearlyReport::YearlyReport()
     mainModel = new QStandardItemModel();
 
     mainView->setModel(mainModel);
+
+    dbHandler = new DatabaseHandler("netatmo-w-analysis/netatmo_analysis.db");
 
     mainLayout = new QHBoxLayout();
     optionsLayout = new QVBoxLayout();
@@ -22,7 +25,16 @@ YearlyReport::YearlyReport()
 void YearlyReport::fillBoard() {
     for (QDate date = QDate(2000, 1, 1); date <= QDate(2000, 12, 31); date = date.addDays(1)) {
         int row = date.dayOfYear();
-        mainModel->setItem(row, 0, new QStandardItem());
+        qDebug() << getMaxTemperatureByDate(date.day(), date.month());
+        mainModel->setItem(row, 0, new QStandardItem(getMaxTemperatureByDate(date.day(), date.month()).toString()));
         mainModel->setVerticalHeaderItem(row, new QStandardItem(date.toString("dd/MM")));
     }
+}
+
+QVariant YearlyReport::getMaxTemperatureByDate(int day, int month) {
+    return dbHandler->getResultFromDatabase(
+                "SELECT maxTemperature, year FROM OutdoorDailyRecords "
+                "WHERE maxTemperature = ("
+                "SELECT max(maxTemperature) FROM OutdoorDailyRecords "
+                "WHERE day = " + QString::number(day) + " AND month = " + QString::number(month) + ")");
 }
