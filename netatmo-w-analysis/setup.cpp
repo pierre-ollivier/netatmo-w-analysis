fe5daf3368c13b0304e3ef233dda8e91a89ec109
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-26 22:01:30 +0200

    Update the version number prior to release

--- netatmo-w-analysis/setup.cpp
+++ netatmo-w-analysis/setup.cpp
@@ -1,5 +1,5 @@
 #include <QString>
 
-extern const QString VERSION = "1.1.0";
-extern const QString RELEASE_DATE = "14/05/2023";
+extern const QString VERSION = "1.2.0";
+extern const QString RELEASE_DATE = "26/05/2023";
 extern const QString PATH_TO_PROD_DATABASE = "netatmo-w-analysis/netatmo_analysis.db";
