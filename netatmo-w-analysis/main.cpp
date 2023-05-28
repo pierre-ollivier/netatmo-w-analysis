8b0a97b270fafdfaa7fc95f073d4231a299b2766
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-28 22:29:43 +0200

    Create a local copy of the main database during the execution of the program and delete it at the end

--- netatmo-w-analysis/main.cpp
+++ netatmo-w-analysis/main.cpp
@@ -1,13 +1,31 @@
 #include <QApplication>
+#include <QFile>
 #include "frontend/MainWindow.h"
 #include "playground.cpp"
 
+extern const QString VERSION;
+extern const QString PATH_TO_PROD_DATABASE;
+extern const QString PATH_TO_COPY_DATABASE;
+extern const QString APP_PATH;
+
 int main(int argc, char *argv[]) {
-    extern const QString VERSION;
     QApplication app(argc, argv);
     MainWindow mainWin;
     mainWin.show();
     mainWin.setWindowTitle("netatmo-w-analysis v" + VERSION);
+
     executeAllPlaygroundFunctions();
-    return app.exec();
+
+    bool valid = QFile::copy(APP_PATH + "/" + PATH_TO_PROD_DATABASE,
+                             APP_PATH + "/" + PATH_TO_COPY_DATABASE);
+
+    if (valid) {
+        int result = app.exec();
+        QFile(APP_PATH + "/" + PATH_TO_COPY_DATABASE).remove();
+        return result;
+    }
+
+    else {
+        return 1;
+    }
 }
--- netatmo-w-analysis/setup.cpp
+++ netatmo-w-analysis/setup.cpp
@@ -3,3 +3,5 @@
 extern const QString VERSION = "1.2.1";
 extern const QString RELEASE_DATE = "27/05/2023";
 extern const QString PATH_TO_PROD_DATABASE = "netatmo-w-analysis/netatmo_analysis.db";
+extern const QString PATH_TO_COPY_DATABASE = "netatmo-w-analysis/netatmo_analysis_copy.db";
+extern const QString APP_PATH = "D:/Mes programmes/netatmo-w-analysis";
