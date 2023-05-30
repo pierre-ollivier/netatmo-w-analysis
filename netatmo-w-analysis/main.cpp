78f726b6e459f72d5f7fcf7ca05007afab773373
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-05-30 22:35:37 +0200

    Add a warning if the copy database couldn't be deleted at the end of the execution

--- netatmo-w-analysis/main.cpp
+++ netatmo-w-analysis/main.cpp
@@ -21,8 +21,12 @@ int main(int argc, char *argv[]) {
 
     if (valid) {
         int result = app.exec();
-        QFile(APP_PATH + "/" + PATH_TO_COPY_DATABASE).remove();
-        return result;
+        bool deleteValid = QFile(APP_PATH + "/" + PATH_TO_COPY_DATABASE).remove();
+        if (deleteValid) return result;
+        else {
+            qDebug() << "Warning: the copied database couldn't be deleted.";
+            return 2;
+        }
     }
 
     else {
