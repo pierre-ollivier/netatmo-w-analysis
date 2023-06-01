401e124818febd67b57e7507c54832e00954facc
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-01 21:23:45 +0200

    More detailed error message when the database copy cannot be deleted

--- netatmo-w-analysis/main.cpp
+++ netatmo-w-analysis/main.cpp
@@ -21,10 +21,12 @@ int main(int argc, char *argv[]) {
 
     if (valid) {
         int result = app.exec();
-        bool deleteValid = QFile(APP_PATH + "/" + PATH_TO_COPY_DATABASE).remove();
+        QFile copyDatabase(APP_PATH + "/" + PATH_TO_COPY_DATABASE);
+        bool deleteValid = copyDatabase.remove();
         if (deleteValid) return result;
         else {
             qDebug() << "Warning: the copied database couldn't be deleted.";
+            qDebug() << copyDatabase.errorString();
             return 2;
         }
     }
