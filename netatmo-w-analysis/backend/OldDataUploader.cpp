aad25de2f6b1c5cc18dc7d2bf183c63d08dae725
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-04 18:50:11 +0200

    Start to remove legacy code

--- netatmo-w-analysis/backend/OldDataUploader.cpp
+++ netatmo-w-analysis/backend/OldDataUploader.cpp
@@ -13,37 +13,13 @@ OldDataUploader::OldDataUploader(NetatmoAPIHandler* apiHandler, QString accessTo
             SLOT(addExtTimestampRecordToCopyDatabase(ExtTimestampRecord)));
     connect(apiHandler, SIGNAL(intTimestampRecordRetrieved(IntTimestampRecord)),
             SLOT(addIntTimestampRecordToCopyDatabase(IntTimestampRecord)));
-    connect(apiHandler, SIGNAL(endOfDailyIndoorConditionsRetrieval()), SLOT(intBatchRetrieved()));
-    connect(apiHandler, SIGNAL(endOfDailyOutdoorConditionsRetrieval()), SLOT(extBatchRetrieved()));
     connect(apiHandler, SIGNAL(extDailyRecordRetrieved(ExtDailyRecord)), SLOT(logExtDailyRecord(ExtDailyRecord)));
 }
 
 void OldDataUploader::addDataFromCurrentMonths(QDate beginDate, QDate endDate, bool indoor) {
     qDebug() << beginDate << endDate;
     if (_accessToken == "") qDebug() << "Warning: undefined access token in OldDataUploader";
-    int N = beginDate.daysTo(endDate);
     _beginDate = beginDate; _endDate = endDate;
-
-//    for (int n = 0; n <= N; n += 3) {
-//        if (indoor) numberOfIntBatchesToRetrieve += 1; else numberOfExtBatchesToRetrieve += 1;
-
-//        long long beginTimestamp = QDateTime(beginDate.addDays(n)).toSecsSinceEpoch();
-//        long long endTimestamp = QDateTime(beginDate.addDays(n + 2)).toSecsSinceEpoch() + 86400;
-
-//        if (n == 0) { // first day: we add the hours of the previous day after 18 UTC
-//            beginTimestamp = QDateTime(beginDate.addDays(-1), QTime(18, 0), Qt::UTC).toSecsSinceEpoch();
-//        }
-//        if (n + 3 > N) { //last day: we add the hours of the next day before 6 UTC
-//            endTimestamp = QDateTime(endDate.addDays(1), QTime(6, 0), Qt::UTC).toSecsSinceEpoch();
-//        }
-
-//        if (indoor) {
-//            _apiHandler->postIndoorDailyRequest(beginTimestamp, endTimestamp, "max", _accessToken);
-//        }
-//        else {
-//            _apiHandler->postOutdoorDailyRequest(beginTimestamp, endTimestamp, "max", _accessToken);
-//        }
-//    }
 
     long long beginTimestamp = QDateTime(beginDate).toSecsSinceEpoch();
     long long endTimestamp = QDateTime(endDate).toSecsSinceEpoch();
@@ -62,23 +38,6 @@ void OldDataUploader::addExtTimestampRecordToCopyDatabase(ExtTimestampRecord rec
 void OldDataUploader::addIntTimestampRecordToCopyDatabase(IntTimestampRecord record) {
     DatabaseHandler dbHandlerCopy(PATH_TO_COPY_DATABASE);
     dbHandlerCopy.postIndoorTimestampRecord(record, "IndoorTimestampRecords");
-}
-
-void OldDataUploader::extBatchRetrieved() {
-    DatabaseHandler dbHandlerCopy(PATH_TO_COPY_DATABASE);
-    numberOfExtBatchesToRetrieve--;
-    if (numberOfExtBatchesToRetrieve == 0) {
-//        dbHandlerCopy.updateOutdoorDailyRecords(_beginDate, _endDate, false);
-    }
-}
-
-void OldDataUploader::intBatchRetrieved() {
-    DatabaseHandler dbHandlerCopy(PATH_TO_COPY_DATABASE);
-    numberOfIntBatchesToRetrieve--;
-    qDebug() << numberOfIntBatchesToRetrieve;
-    if (numberOfIntBatchesToRetrieve == 0) {
-//        dbHandlerCopy.updateIndoorDailyRecords(_beginDate, _endDate, false);
-    }
 }
 
 void OldDataUploader::setAccessToken(QString accessToken) {
