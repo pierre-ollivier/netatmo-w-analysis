a98c37a5acf69e6a7b079a190af3d75787f5ca1c
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-06 21:53:47 +0200

    Auto-formatting of the x-axis every 30 minutes

--- netatmo-w-analysis/frontend/HomePageChart.cpp
+++ netatmo-w-analysis/frontend/HomePageChart.cpp
@@ -10,16 +10,16 @@ HomePageChart::HomePageChart(QString tableName) : QChartView()
 
     dbHandler = new DatabaseHandler(PATH_TO_COPY_DATABASE);
 
-    series = new QLineSeries();
-    fillSeries();
-
     xAxis = new QDateTimeAxis();
     xAxis->setFormat("hh:mm");
-    xAxis->setRange(
-                QDateTime(QDate(2023, 5, 31), QTime(20, 0)),
-                QDateTime(QDate(2023, 6, 1), QTime(0, 0)));
+//    xAxis->setRange(
+//                QDateTime(QDate(2023, 5, 31), QTime(20, 0)),
+//                QDateTime(QDate(2023, 6, 1), QTime(0, 0)));
     // provisional - range needs to be adapted to the scale
     xAxis->setTickCount(5);
+
+    series = new QLineSeries();
+    fillSeries();
 
     yAxis = new QValueAxis();
     yAxis->setLabelFormat("%.1f");
@@ -39,11 +39,15 @@ HomePageChart::HomePageChart(QString tableName) : QChartView()
 }
 
 void HomePageChart::fillSeries() {
-//    QString timelimit = QString::number(QDateTime::currentDateTime().toSecsSinceEpoch() - 4 * 3600);
-    QString timelimit = QString::number(1685556000);
+//    int inttimelimit = QString::number(QDateTime::currentDateTime().toSecsSinceEpoch() - 4 * 3600);
+    int inttimelimit = 1685556000;
+    int chartStart = inttimelimit - (inttimelimit % 1800);
+    QString timelimit = QString::number(inttimelimit);
     std::vector<ExtTimestampRecord> records = dbHandler->getExtTimestampRecordsFromDatabase(
                 "SELECT * from " + _tableName + " where timestamp >= " + timelimit, 0);
     for (ExtTimestampRecord record: records) {
         series->append(1000 * record.timestamp(), record.temperature());
     }
+    xAxis->setRange(QDateTime::fromSecsSinceEpoch(chartStart),
+                    QDateTime::fromSecsSinceEpoch(chartStart + 4 * 3600));
 }
