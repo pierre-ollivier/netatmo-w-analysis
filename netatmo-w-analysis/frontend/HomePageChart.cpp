30699a1dbc4d1482370ce3f9fb8bf7e04f87af9d
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-09 21:58:42 +0200

    Remove legacy code

--- netatmo-w-analysis/frontend/HomePageChart.cpp
+++ netatmo-w-analysis/frontend/HomePageChart.cpp
@@ -13,10 +13,6 @@ HomePageChart::HomePageChart(NetatmoAPIHandler *apiHandler, QString tableName) :
 
     xAxis = new QDateTimeAxis();
     xAxis->setFormat("hh:mm");
-//    xAxis->setRange(
-//                QDateTime(QDate(2023, 5, 31), QTime(20, 0)),
-//                QDateTime(QDate(2023, 6, 1), QTime(0, 0)));
-    // provisional - range needs to be adapted to the scale
     xAxis->setTickCount(5);
 
     QString unitWithTrailingSpace = " °C";
@@ -25,7 +21,6 @@ HomePageChart::HomePageChart(NetatmoAPIHandler *apiHandler, QString tableName) :
     yAxis->setTickType(QValueAxis::TicksDynamic);
 
     series = new QLineSeries();
-//    fillSeries();
 
     chart = new QChart();
 
@@ -56,19 +51,8 @@ void HomePageChart::gatherChartData(QString accessToken) {
 }
 
 void HomePageChart::drawChart(QList<QPointF> points) {
-//    int inttimelimit = QString::number(QDateTime::currentDateTime().toSecsSinceEpoch() - 4 * 3600);
-    int inttimelimit = 1685556000;
-    int chartStart = inttimelimit - (inttimelimit % 1800);
-    QString timelimit = QString::number(inttimelimit);
-
     maxOfSeries = QVariant();
     minOfSeries = QVariant();
-
-//    for (ExtTimestampRecord record: records) {
-//        series->append(1000 * record.timestamp(), record.temperature());
-//        if (maxOfSeries.isNull() || record.temperature() > maxOfSeries.toDouble()) maxOfSeries = record.temperature();
-//        if (minOfSeries.isNull() || record.temperature() < minOfSeries.toDouble()) minOfSeries = record.temperature();
-//    }
 
     series->clear();
     series->append(points);
