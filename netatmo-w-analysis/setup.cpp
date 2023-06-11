36c6aa2f09f739a932b539f2e7361442f016b2b2
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-11 22:08:16 +0200

    Change version number

--- netatmo-w-analysis/setup.cpp
+++ netatmo-w-analysis/setup.cpp
@@ -1,7 +1,7 @@
 #include <QString>
 
-extern const QString VERSION = "1.3.0";
-extern const QString RELEASE_DATE = "04/06/2023";
+extern const QString VERSION = "1.4.0";
+extern const QString RELEASE_DATE = "11/06/2023";
 extern const QString PATH_TO_PROD_DATABASE = "netatmo-w-analysis/netatmo_analysis.db";
 extern const QString PATH_TO_COPY_DATABASE = "netatmo-w-analysis/netatmo_analysis_copy.db";
 extern const QString APP_PATH = "D:/Mes programmes/netatmo-w-analysis";
