dcb0df6b762fcf1bcbf511027bbe8ba193473733
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-13 22:38:56 +0200

    Handle the case of start date and end date being the same year

--- netatmo-w-analysis/backend/NormalComputer.cpp
+++ netatmo-w-analysis/backend/NormalComputer.cpp
@@ -6,9 +6,17 @@ NormalComputer::NormalComputer(DatabaseHandler *dbHandler)
 }
 
 double NormalComputer::normalMaximalTemperatureByMovingAverage(QString tableName, QDate date, int daysCount) {
-    QString query = "SELECT AVG(maxTemperature) FROM " + tableName;
+    QString query = "SELECT AVG(maxTemperature) FROM " + tableName + " ";
     int dayGap = daysCount / 2; // number of days on each side
     QDate beginDate = date.addDays(-dayGap), endDate = date.addDays(dayGap);
 
     // TODO
+    if (beginDate.year() == endDate.year()) {
+        query += "WHERE 100*month + day BETWEEN "
+                + QString::number(100 * beginDate.month() + beginDate.day())
+                + " AND "
+                + QString::number(100 * endDate.month() + endDate.day());
+    }
+
+    return _dbHandler->getResultFromDatabase(query).toDouble();
 }
