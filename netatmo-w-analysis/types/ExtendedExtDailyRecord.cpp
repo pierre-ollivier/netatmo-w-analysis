b90be5e1835abb1d8c12c2c8b4f111ba5412c1ca
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-19 21:41:11 +0200

    Create the new type ExtendedExtDailyRecord

---
+++ netatmo-w-analysis/types/ExtendedExtDailyRecord.cpp
@@ -0,0 +1,48 @@
+#include "ExtendedExtDailyRecord.h"
+
+ExtendedExtDailyRecord::ExtendedExtDailyRecord()
+{
+
+}
+
+ExtDailyRecord ExtendedExtDailyRecord::extDailyRecord() {
+    return *_extDailyRecord;
+}
+double ExtendedExtDailyRecord::minTemperature() {
+    return _minTemperature;
+}
+double ExtendedExtDailyRecord::maxTemperature() {
+    return _maxTemperature;
+}
+bool ExtendedExtDailyRecord::extDailyRecordDefined() {
+    return _extDailyRecordDefined;
+}
+bool ExtendedExtDailyRecord::minTemperatureDefined() {
+    return _minTemperatureDefined;
+}
+bool ExtendedExtDailyRecord::maxTemperatureDefined() {
+    return _maxTemperatureDefined;
+}
+bool ExtendedExtDailyRecord::allDefined() {
+    return extDailyRecordDefined() && maxTemperatureDefined() && minTemperatureDefined();
+}
+
+void ExtendedExtDailyRecord::setExtDailyRecord(ExtDailyRecord record) {
+    _extDailyRecord = &record;
+    _extDailyRecordDefined = true;
+}
+void ExtendedExtDailyRecord::setMinTemperature(double minTemperature) {
+    _minTemperature = minTemperature;
+    _minTemperatureDefined = true;
+}
+void ExtendedExtDailyRecord::setMaxTemperature(double maxTemperature) {
+    _maxTemperature = maxTemperature;
+    _maxTemperatureDefined = true;
+}
+
+ExtDailyRecord ExtendedExtDailyRecord::wrap() {
+    ExtDailyRecord *result = new ExtDailyRecord(*_extDailyRecord);
+    result->setMaxTemperature(_maxTemperature);
+    result->setMinTemperature(_minTemperature);
+    return *result;
+}
---
+++ netatmo-w-analysis/types/ExtendedExtDailyRecord.h
@@ -0,0 +1,34 @@
+#ifndef EXTENDEDEXTDAILYRECORD_H
+#define EXTENDEDEXTDAILYRECORD_H
+
+#include "../netatmo-w-analysis/types/ExtDailyRecord.h"
+
+class ExtendedExtDailyRecord
+{
+public:
+    ExtendedExtDailyRecord();
+
+    ExtDailyRecord extDailyRecord();
+    double minTemperature();
+    double maxTemperature();
+    bool extDailyRecordDefined();
+    bool minTemperatureDefined();
+    bool maxTemperatureDefined();
+    bool allDefined();
+
+    void setExtDailyRecord(ExtDailyRecord record);
+    void setMinTemperature(double minTemperature);
+    void setMaxTemperature(double maxTemperature);
+
+    ExtDailyRecord wrap();
+
+private:
+    ExtDailyRecord *_extDailyRecord = new ExtDailyRecord(QDate(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+    double _minTemperature = 0;
+    double _maxTemperature = 0;
+    bool _extDailyRecordDefined = false;
+    bool _maxTemperatureDefined = false;
+    bool _minTemperatureDefined = false;
+};
+
+#endif // EXTENDEDEXTDAILYRECORD_H
