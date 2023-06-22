c1f2087f69adb944b645898cb3dd99f6b3bfa3d1
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-22 20:02:42 +0200

    Format the labels with font and color

--- netatmo-w-analysis/frontend/EphemerisPanel.cpp
+++ netatmo-w-analysis/frontend/EphemerisPanel.cpp
@@ -13,9 +13,9 @@ EphemerisPanel::EphemerisPanel() : QGroupBox()
 
     layout = new QGridLayout();
 
-    layout->addWidget(new QLabel("Température maximale : "), 1, 1);
+    layout->addWidget(new QLabel("Température maximale absolue : "), 1, 1);
     layout->addWidget(txxLabel, 1, 2);
-    layout->addWidget(new QLabel("Température minimale : "), 2, 1);
+    layout->addWidget(new QLabel("Température minimale absolue : "), 2, 1);
     layout->addWidget(tnnLabel, 2, 2);
     layout->addWidget(new QLabel("Température maximale moyenne : "), 3, 1);
     layout->addWidget(txmLabel, 3, 2);
@@ -26,6 +26,7 @@ EphemerisPanel::EphemerisPanel() : QGroupBox()
     normalComputer = new NormalComputer(dbHandler);
 
     setTitle("Statistiques pour un _ _");
+    setFont(QFont("Arial", 13));
     setLayout(layout);
     setAlignment(Qt::AlignHCenter);
 }
@@ -37,25 +38,25 @@ void EphemerisPanel::setDate(QDate date) {
                 "SELECT max(maxTemperature) FROM OutdoorDailyRecords "
                 "WHERE month = " + QString::number(date.month()) + " "
                 "AND day = " + QString::number(date.day())).toDouble();
-    txxLabel->setText(deviceLocale->toString(txx, 'f', 1) + " °C");
+    txxLabel->setText("<font color = \"#209fdf\">" + deviceLocale->toString(txx, 'f', 1) + "</font> °C");
 
     double tnn = dbHandler->getResultFromDatabase(
                 "SELECT min(minTemperature) FROM OutdoorDailyRecords "
                 "WHERE month = " + QString::number(date.month()) + " "
                 "AND day = " + QString::number(date.day())).toDouble();
-    tnnLabel->setText(deviceLocale->toString(tnn, 'f', 1) + " °C");
+    tnnLabel->setText("<font color = \"#209fdf\">" + deviceLocale->toString(tnn, 'f', 1) + "</font> °C");
 
     double txm = normalComputer->normalMeasurementByMovingAverage(
                 "OutdoorDailyRecords",
                 date,
                 "maxTemperature",
                 41);
-    txmLabel->setText(deviceLocale->toString(txm, 'f', 1) + " °C");
+    txmLabel->setText("<font color = \"#209fdf\">" + deviceLocale->toString(txm, 'f', 1) + "</font> °C");
 
     double tnm = normalComputer->normalMeasurementByMovingAverage(
                 "OutdoorDailyRecords",
                 date,
                 "minTemperature",
                 41);
-    tnmLabel->setText(deviceLocale->toString(tnm, 'f', 1) + " °C");
+    tnmLabel->setText("<font color = \"#209fdf\">" + deviceLocale->toString(tnm, 'f', 1) + "</font> °C");
 }
