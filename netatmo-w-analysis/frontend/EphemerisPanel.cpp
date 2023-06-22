a1b585e811bfb321b14ec2d1387d0d281715b86d
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-22 19:32:02 +0200

    Add the display of the maximal temperature

--- netatmo-w-analysis/frontend/EphemerisPanel.cpp
+++ netatmo-w-analysis/frontend/EphemerisPanel.cpp
@@ -1,8 +1,19 @@
 #include "EphemerisPanel.h"
 
+extern const QString PATH_TO_PROD_DATABASE;
+
 EphemerisPanel::EphemerisPanel() : QGroupBox()
 {
+    txxLabel = new QLabel("__,_ °C");
+
+    deviceLocale = new QLocale();
+
     layout = new QGridLayout();
+
+    layout->addWidget(new QLabel("Température maximale : "), 1, 1);
+    layout->addWidget(txxLabel, 1, 2);
+
+    dbHandler = new DatabaseHandler(PATH_TO_PROD_DATABASE);
 
     setTitle("Statistiques pour un _ _");
     setLayout(layout);
@@ -11,4 +22,9 @@ EphemerisPanel::EphemerisPanel() : QGroupBox()
 
 void EphemerisPanel::setDate(QDate date) {
     setTitle("Statistiques pour un " + date.toString("dd MMMM"));
+    double txx = dbHandler->getResultFromDatabase(
+                "SELECT max(maxTemperature) FROM OutdoorDailyRecords "
+                "WHERE month = " + QString::number(date.month()) + " "
+                "AND day = " + QString::number(date.day())).toDouble();
+    txxLabel->setText(deviceLocale->toString(txx, 'f', 1) + " °C");
 }
