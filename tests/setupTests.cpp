f0958475bab0c1f5787600a52bd1eab2a4d7f20f
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-06-23 22:34:34 +0200

    Create a global variable PATH_TO_TEST_DATABASE

--- tests/setupTests.cpp
+++ tests/setupTests.cpp
@@ -2,3 +2,4 @@
 
 extern const QString PATH_TO_PROD_DATABASE = "netatmo-w-analysis/testdb.db";
 extern const QString PATH_TO_COPY_DATABASE = "netatmo-w-analysis/testdb.db";
+extern const QString PATH_TO_TEST_DATABASE = "testdb.db";
