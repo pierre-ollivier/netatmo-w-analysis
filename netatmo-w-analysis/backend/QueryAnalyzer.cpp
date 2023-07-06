882ef340c50294f4adc4246fb1fd7e91263f75eb
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-07-06 21:11:06 +0200

    Fix dateQueryFromMeasurementQuery

--- netatmo-w-analysis/backend/QueryAnalyzer.cpp
+++ netatmo-w-analysis/backend/QueryAnalyzer.cpp
@@ -6,9 +6,9 @@ QueryAnalyzer::QueryAnalyzer()
 }
 
 QString QueryAnalyzer::dateQueryFromMeasurementQuery(QString measurementQuery) {
-    measurementQuery = measurementQuery.toLower();
-    int indexOfSelect = measurementQuery.indexOf("select");
-    int indexOfFrom = measurementQuery.indexOf("from");
+    QString measurementQueryLowered = measurementQuery.toLower();
+    int indexOfSelect = measurementQueryLowered.indexOf("select");
+    int indexOfFrom = measurementQueryLowered.indexOf("from");
     if (indexOfSelect >= 0 && indexOfFrom >= 0) {
         return "SELECT date FROM" + measurementQuery.right(measurementQuery.size() - indexOfFrom - 4);
     }
