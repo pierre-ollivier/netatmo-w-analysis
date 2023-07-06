8c8a8576266277d32868001f6133c022893ca9c2
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-07-06 21:18:48 +0200

    Add first tests for toASC

--- tests/TestQueryAnalyzer.cpp
+++ tests/TestQueryAnalyzer.cpp
@@ -18,7 +18,23 @@ void TestQueryAnalyzer::testDateQueryFromMeasurementQuery() {
              "SELECT date FROM IndoorDailyRecords ORDER BY ((maxTemperature - minTemperature)/2)");
 }
 void TestQueryAnalyzer::testToASC() {
+    QCOMPARE(analyzer->toASC(
+                 "SELECT ((maxTemperature - minTemperature)/2) FROM IndoorDailyRecords "
+                 "ORDER BY ((maxTemperature - minTemperature)/2)"),
+             "select ((maxTemperature - minTemperature)/2) from IndoorDailyRecords "
+             "order by ((maxTemperature - minTemperature)/2) asc");
 
+    QCOMPARE(analyzer->toASC(
+                 "SELECT ((maxTemperature - minTemperature)/2) FROM IndoorDailyRecords "
+                 "ORDER BY ((maxTemperature - minTemperature)/2) ASC"),
+             "select ((maxTemperature - minTemperature)/2) from IndoorDailyRecords "
+             "order by ((maxTemperature - minTemperature)/2) asc");
+
+    QCOMPARE(analyzer->toASC(
+                 "SELECT ((maxTemperature - minTemperature)/2) FROM IndoorDailyRecords "
+                 "ORDER BY ((maxTemperature - minTemperature)/2) DESC"),
+             "select ((maxTemperature - minTemperature)/2) from IndoorDailyRecords "
+             "order by ((maxTemperature - minTemperature)/2) asc");
 }
 void TestQueryAnalyzer::testToDESC() {
 
