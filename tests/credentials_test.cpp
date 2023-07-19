bb9744f2151fac97b6e5cd8afc8790e31024834d
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-07-19 13:12:58 +0200

    Remove ignored file

--- tests/credentials_test.cpp
+++
@@ -1,9 +0,0 @@
-// While modifying this file, make sure to modify as well its copy in main folder
-
-#include <QString>
-
-extern const QString clientId = "5fde695c33960f49ba234885";
-extern const QString clientSecret = "nfZBPf2C7O1klQTHNWcs0Efohi2mg3CXmA";
-extern const QString mainDeviceId = "70%3Aee%3A50%3A52%3Ac0%3A40";
-extern const QString indoorModuleId = "70%3Aee%3A50%3A52%3Ac0%3A40";
-extern const QString outdoorModuleId = "02%3A00%3A00%3A3f%3A01%3Ae4";
