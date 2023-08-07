0146c5699f74bffd4709b02a6fa3310e04ead13f
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-08-07 20:32:30 +0200

    Move to OpenWeatherMap's API and make it work

--- netatmo-w-analysis/backend/WeatherAPIHandler.cpp
+++ netatmo-w-analysis/backend/WeatherAPIHandler.cpp
@@ -9,33 +9,27 @@ WeatherAPIHandler::WeatherAPIHandler()
 }
 
 void WeatherAPIHandler::postWeatherRequest() {
-    extern const QString infoclimatAuth;
-    extern const QString infoclimatC;
+    extern const QString longitude;
+    extern const QString latitude;
     extern const QString openWeatherMapAPIKey;
-//    QUrl url("https://www.infoclimat.fr/public-api/gfs/json");
-    QUrl url("api.openweathermap.org/data/2.5/forecast");
-    QNetworkRequest request(url);
 
-    request.setHeader(QNetworkRequest::ContentTypeHeader, "application/x-www-form-urlencoded");
-
+    QUrl url("https://api.openweathermap.org/data/2.5/forecast");
     QUrlQuery params;
-//    params.addQueryItem("_ll", "48.894410,2.336951");
-//    params.addQueryItem("_auth", infoclimatAuth);
-//    params.addQueryItem("_c", infoclimatC);
-//    params.addQueryItem("%5Fll", "48.894410,2.336951");
-//    params.addQueryItem("%5Fauth", infoclimatAuth);
-//    params.addQueryItem("%5Fc", infoclimatC);
-    params.addQueryItem("lat", "48.894410");
-    params.addQueryItem("lon", "2.336951");
+
+    params.addQueryItem("lat", latitude);
+    params.addQueryItem("lon", longitude);
     params.addQueryItem("appid", openWeatherMapAPIKey);
-    qDebug() << params.query().toUtf8();
-    manager->post(request, params.query().toUtf8());
+
+    url.setQuery(params);
+
+    QNetworkRequest request(url);
+    manager->get(request);
 }
 
 void WeatherAPIHandler::retrieveWeather(QNetworkReply *reply) {
     QByteArray bytes = reply->readAll();
     if (bytes.contains("error")) {
-        qDebug() << "ERROR with current conditions" << bytes;
+        qDebug() << "ERROR with weather API" << bytes;
     }
     else if (bytes.size() >= 1) {
         qDebug() << bytes;
