30c92391b5251fb803ed5f7dec8d0b326ce7fa63
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-08-08 21:18:58 +0200

    Add temperature, humidity and pressure

--- netatmo-w-analysis/types/PredictionItem.cpp
+++ netatmo-w-analysis/types/PredictionItem.cpp
@@ -7,6 +7,23 @@ PredictionItem::PredictionItem()
 
 PredictionItem::PredictionItem(QJsonObject jsonObject) {
     _dateTime = QDateTime::fromSecsSinceEpoch(jsonObject["dt"].toInt());
+
+    QJsonObject mainObject = jsonObject["main"].toObject();
+    _temperature = mainObject["temp"].toDouble();
+    _feltTemperature = mainObject["feels_like"].toDouble();
+    _minTemperature = mainObject["temp_min"].toDouble();
+    _maxTemperature = mainObject["temp_max"].toDouble();
+    _pressure = mainObject["pressure"].toDouble();
+
+    _humidity = mainObject["humidity"].toInt();
 }
 
 QDateTime PredictionItem::dateTime() {return _dateTime;}
+
+double PredictionItem::temperature() {return _temperature;}
+double PredictionItem::feltTemperature() {return _feltTemperature;}
+double PredictionItem::minTemperature() {return _minTemperature;}
+double PredictionItem::maxTemperature() {return _maxTemperature;}
+double PredictionItem::pressure() {return _pressure;}
+
+int PredictionItem::humidity() {return _humidity;}
