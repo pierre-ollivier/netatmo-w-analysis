d0a48efafec9e016862e91a37fcd65024df3bf9e
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-08-30 19:21:29 +0200

    A very first PredictionWidget for demonstration purposes

--- netatmo-w-analysis/frontend/PredictionWidget/PredictionWidget.h
+++ netatmo-w-analysis/frontend/PredictionWidget/PredictionWidget.h
@@ -1,6 +1,8 @@
 #ifndef PREDICTIONWIDGET_H
 #define PREDICTIONWIDGET_H
 
+#include <QGridLayout>
+#include <QLabel>
 #include <QWidget>
 
 class PredictionWidget : public QWidget
@@ -9,7 +11,18 @@ class PredictionWidget : public QWidget
 public:
     explicit PredictionWidget(QWidget *parent = nullptr);
 
+public slots:
+    void setMaximumTemperature(int maxTemperature);
+    void setMinimumTemperature(int minTemperature);
+
 private:
+    QLabel *maxTemperatureLabel;
+    QLabel *minTemperatureLabel;
+
+    QGridLayout *layout;
+
+    int _maxTemperature = 0;
+    int _minTemperature = 0;
 
 };
 
