cb4b180b6039e8c15bd66b910e602f4fe9e43aee
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-09-01 13:54:21 +0200

    Add blue/red arrows in PredictionWidget

--- netatmo-w-analysis/frontend/PredictionWidget/PredictionWidget.cpp
+++ netatmo-w-analysis/frontend/PredictionWidget/PredictionWidget.cpp
@@ -14,10 +14,12 @@ PredictionWidget::PredictionWidget(QWidget *parent) : QGroupBox(parent)
 
 void PredictionWidget::setMaximumTemperature(double maxTemperature) {
     _maxTemperature = maxTemperature;
-    maxTemperatureLabel->setText(QString::number(_maxTemperature, 'f', 0) + " °C");
+    maxTemperatureLabel->setText("<font color=\"#ff1000\">↑ </font>"
+                                 + QString::number(_maxTemperature, 'f', 0) + " °C");
 }
 
 void PredictionWidget::setMinimumTemperature(double minTemperature) {
     _minTemperature = minTemperature;
-    minTemperatureLabel->setText(QString::number(_minTemperature, 'f', 0) + " °C");
+    minTemperatureLabel->setText("<font color=\"#0010ff\">↓ </font>"
+                                 + QString::number(_minTemperature, 'f', 0) + " °C");
 }
