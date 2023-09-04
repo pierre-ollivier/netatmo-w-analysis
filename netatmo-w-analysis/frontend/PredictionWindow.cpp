3bb5e7f69b03db3090f6c18bb7d6437b0917a34d
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-09-04 21:39:48 +0200

    Make the prediction window actually show something

--- netatmo-w-analysis/frontend/PredictionWindow.cpp
+++ netatmo-w-analysis/frontend/PredictionWindow.cpp
@@ -14,10 +14,23 @@ PredictionWindow::PredictionWindow(QWidget *parent) : QWidget(parent)
 
 PredictionWindow::PredictionWindow(WeatherPrediction *weatherPrediction) : QWidget()
 {
-    PredictionWindow();
+    layout = new QGridLayout();
+
+    for (int i = 0; i < 4; i++) {
+        predictionWidgets[i] = new PredictionWidget();
+        layout->addWidget(predictionWidgets[i], 1, i);
+    }
+
+    setLayout(layout);
     setWeatherPrediction(weatherPrediction);
 }
 
 void PredictionWindow::setWeatherPrediction(WeatherPrediction *weatherPrediction) {
     _weatherPrediction = weatherPrediction;
+    for (int i = 0; i < 4; i++) {
+        predictionWidgets[i]->setMaximumTemperature(weatherPrediction->maxTemperature(i + 1));
+        predictionWidgets[i]->setMinimumTemperature(weatherPrediction->minTemperature(i + 1));
+        predictionWidgets[i]->setPictogram(weatherPrediction->dayPictogram(i + 1));
+        predictionWidgets[i]->setTitle(QDate::currentDate().addDays(i + 1).toString("d MMMM"));
+    }
 }
