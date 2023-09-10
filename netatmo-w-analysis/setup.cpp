1b0fd608eff51928ba6cbbc366742526b2803c1b
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2023-09-10 22:31:59 +0200

    Bump version number

--- netatmo-w-analysis/setup.cpp
+++ netatmo-w-analysis/setup.cpp
@@ -2,8 +2,8 @@
 #include <QFont>
 #include <QString>
 
-extern const QString VERSION = "2.5.0";
-extern const QString RELEASE_DATE = "04/08/2023";
+extern const QString VERSION = "2.6.0";
+extern const QString RELEASE_DATE = "10/09/2023";
 extern const QString PATH_TO_PROD_DATABASE = "netatmo-w-analysis/netatmo_analysis.db";
 extern const QString PATH_TO_COPY_DATABASE = "netatmo-w-analysis/netatmo_analysis_copy.db";
 extern const QString APP_PATH = "D:/Mes programmes/netatmo-w-analysis";
