afc80299e0ee1739a884641b1f62a69c1d11dc3d
Pierre Ollivier <pierre.ollivier2001@gmail.com>
2024-07-16 13:50:27 +0200

    Better handle equal and almost equal values in DataExplorator

--- netatmo-w-analysis/frontend/DataExplorator.cpp
+++ netatmo-w-analysis/frontend/DataExplorator.cpp
@@ -188,7 +188,7 @@ void DataExplorator::fillBoards(QString query) {
                         dataDESC[i].toDouble(), 'f', decimalCount) + unitWithLeadingSpace);
         mainModelMax->item(i, 1)->setText(datesDESC[i].toString());
         if (i >= 1) {
-            if (dataDESC[i] == dataDESC[i - 1]) {
+            if (abs(dataDESC[i].toDouble() - dataDESC[i - 1].toDouble()) < 1e-6) {
                 mainModelMax->verticalHeaderItem(i)->setText(mainModelMax->verticalHeaderItem(i - 1)->text());
             }
         }
@@ -201,7 +201,7 @@ void DataExplorator::fillBoards(QString query) {
                         dataASC[i].toDouble(), 'f', decimalCount) + unitWithLeadingSpace);
         mainModelMin->item(i, 1)->setText(datesASC[i].toString());
         if (i >= 1) {
-            if (dataASC[i] == dataASC[i - 1]) {
+            if (abs(dataASC[i].toDouble() - dataASC[i - 1].toDouble()) < 1e-6) {
                 mainModelMin->verticalHeaderItem(i)->setText(mainModelMin->verticalHeaderItem(i - 1)->text());
             }
         }
